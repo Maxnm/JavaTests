9f451f4eef77a50ea866f32a54edd0f1dcbfe738
Maxnm <mrozhok@gmail.com>
2015-09-20 17:20:54 +0300

    Treemap update

--- Abramyan/src/com/geekbrains/tests/Test3Treemap.java
+++ Abramyan/src/com/geekbrains/tests/Test3Treemap.java
@@ -1,5 +1,6 @@
 package com.geekbrains.tests;
 
+import java.util.Arrays;
 import java.util.TreeMap;
 
 /**
@@ -9,8 +10,33 @@ import java.util.TreeMap;
  * @Purpose Education
  */
 public class Test3Treemap {
+    public static void main(String[] args) {
+        int arr[] = {3, 4, 1, 5, 7, 3, 1, 4, 6, 8, 2};
 
+        TreeMap<Integer, Integer> tm = new TreeMap<>();
+
+        for (int aArr : arr) {
+            int cnt = 1;
+            if (tm.containsKey(aArr)) {
+                cnt = tm.get(aArr);
+                cnt++;
+            }
+            tm.put(aArr, cnt);
+        }
+
+        int j = 0;
+        for (int k : tm.keySet()) {
+            for (int i = 0; i < tm.get(k); i++) {
+                arr[j] = k;
+                j++;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
+}
+
+
 
 
 
